c7bffd34e323350a353b22508a8c9ab19d53f1b9
Karthik <karthik.india90@gmail.com>
2022-01-30 19:45:33 +0530

    Checkin for Paving Category

---
+++ Paving/Controllers/CategoryController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Paving.Data;
+using Paving.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Paving.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+      
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categorylist = _db.Category;
+
+            return View(categorylist);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
